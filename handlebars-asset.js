b5d645d93abadfae13a7d7b797f5f1444ec30cee
Aleksandr Panichenko <nasada@yandex.ru>
2021-06-10 13:21:54 +0300

    toggle to TS

--- handlebars-asset.js
+++ handlebars-asset.js
@@ -1,17 +1,17 @@
-const Handlebars = require('handlebars');
-const JSAsset = require('parcel-bundler/src/assets/JSAsset');
+const Handlebars = require("handlebars");
+const TSAsset = require("parcel-bundler/src/assets/TypeScriptAsset");
 
-class HbsAsset extends JSAsset {
-    async pretransform() {
-        const precompiled = Handlebars.precompile(this.contents);
-        this.contents = `
-                import Handlebars from 'handlebars/dist/handlebars.runtime';
+class HbsAsset extends TSAsset {
+  async pretransform() {
+    const precompiled = Handlebars.precompile(this.contents);
+    this.contents = `
+                import * as Handlebars from 'handlebars/dist/handlebars.runtime';
                 const templateFunction = Handlebars.template(${precompiled});
                 export default templateFunction;
                 `;
 
-        return await super.pretransform();
-    }
+    return await super.pretransform();
+  }
 }
 
 module.exports = HbsAsset;
